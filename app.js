c6b7a458d21c60e174828818b6d08d2858d0e04b
Paras Setia <parassetia889@gmail.com>
2023-10-29 10:42:16 +0530

    Taking input in console

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("working")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!");
+    console.log("input ", txtInput.value);
+})
