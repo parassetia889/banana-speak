587812377ac3d64a46fe3b0f86567042c960dedb
Paras Setia <parassetia889@gmail.com>
2023-10-29 13:51:53 +0530

    Input Output Processing done

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector('#output')
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!");
-    console.log("input ", txtInput.value);
-})
+
+function clickEventHandler(){
+    outputDiv.innerText = "translated : fsfhsjfsj "+ txtInput.value;
+}
+
+
+btnTranslate.addEventListener("click", clickEventHandler)
